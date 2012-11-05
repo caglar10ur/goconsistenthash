844a7275551a5ff86fb058169adfb1b1733ea1cf
S.Çağlar Onur <caglar@10ur.org>
2012-11-05 02:44:25 -0500

    print key distribution while testing

--- consistenthash_test.go
+++ consistenthash_test.go
@@ -2,6 +2,7 @@ package consistenthash
 
 import "testing"
 import "strconv"
+import "fmt"
 
 func TestNew(t *testing.T) {
     numberOfReplicas := 32
@@ -49,34 +50,51 @@ func TestRemoveNonExisting(t *testing.T) {
 
 func TestGet(t *testing.T) {
     numberOfReplicas := 32
+    nodes := []string{"node1", "node2", "node3", "node4"}
     keys := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "ch", "y", "z"}
 
+    m := make(map[string]int)
+
+    distribution := func() {
+        for k, v := range m {
+            fmt.Println(k, v)
+        }
+    }
+
     ch := New(numberOfReplicas)
-
-    ch.Add("node1")
-    ch.Add("node2")
-    ch.Add("node3")
-    ch.Add("node4")
+    for _, v := range nodes {
+        ch.Add(v)
+    }
 
     for _, v := range keys {
-        if (v == "a" && ch.Get(v) != "node1") && (v == "l" && ch.Get(v) != "node2") && (v == "p" && ch.Get(v) != "node3") && (v == "w" && ch.Get(v) != "node4"){
+        m[ch.Get(v)]++
+        if (v == "a" && ch.Get(v) != "node1") || (v == "l" && ch.Get(v) != "node2") || (v == "p" && ch.Get(v) != "node3") || (v == "w" && ch.Get(v) != "node4"){
             t.Errorf("Get failed")
         }
     }
+    distribution()
 
+    fmt.Println()
     ch.Add("node5")
+    m = make(map[string]int)
     for _, v := range keys {
-        if (v == "a" && ch.Get(v) != "node1") && (v == "l" && ch.Get(v) != "node2") && (v == "p" && ch.Get(v) != "node3") && (v == "w" && ch.Get(v) != "node5"){
+        m[ch.Get(v)]++
+        if (v == "a" && ch.Get(v) != "node1") || (v == "l" && ch.Get(v) != "node2") || (v == "p" && ch.Get(v) != "node3") || (v == "w" && ch.Get(v) != "node5"){
             t.Errorf("Get failed")
         }
     }
+    distribution()
 
+    fmt.Println()
     ch.Remove("node5")
+    m = make(map[string]int)
     for _, v := range keys {
-        if (v == "a" && ch.Get(v) != "node1") && (v == "l" && ch.Get(v) != "node2") && (v == "p" && ch.Get(v) != "node3") && (v == "w" && ch.Get(v) != "node4"){
+        m[ch.Get(v)]++
+        if (v == "a" && ch.Get(v) != "node1") || (v == "l" && ch.Get(v) != "node2") || (v == "p" && ch.Get(v) != "node3") || (v == "w" && ch.Get(v) != "node4"){
             t.Errorf("Get failed")
         }
     }
+    distribution()
 }
 
 func BenchmarkCycle(b *testing.B) {
