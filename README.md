a5708e8d7f85930a17a1f2933b45914ea20340b7
S.Çağlar Onur <caglar@10ur.org>
2012-11-04 23:58:12 -0500

    Reimplemantation of http://www.lexemetech.com/2007/11/consistent-hashing.html in Go

---
+++ README.md
@@ -0,0 +1,9 @@
+Reimplemantation of http://www.lexemetech.com/2007/11/consistent-hashing.html in Go
+
+Consistent hashing is a scheme that provides a hash table functionality in a way that the adding or removing of one slot does not significantly change the mapping of keys to slots.
+
+The need for consistent hashing arose from limitations experienced while running collections of caching machines - web caches, for example. If you have a collection of n cache machines then a common way of load balancing across them is to put object o in cache machine number hash(o) mod n. This works well until you add or remove cache machines (for whatever reason), for then n changes and every object is hashed to a new location. This can be catastrophic since the originating content servers are swamped with requests from the cache machines. It's as if the cache suddenly disappeared. Which it has, in a sense. (This is why you should care - consistent hashing is needed to avoid swamping your servers!)
+
+It would be nice if, when a cache machine was added, it took its fair share of objects from all the other cache machines. Equally, when a cache machine was removed, it would be nice if its objects were shared between the remaining machines. This is exactly what consistent hashing does - consistently maps objects to the same cache machine, as far as is possible, at least.
+
+The basic idea behind the consistent hashing algorithm is to hash both objects and caches using the same hash function. The reason to do this is to map the cache to an interval, which will contain a number of object hashes. If the cache is removed then its interval is taken over by a cache with an adjacent interval.
---
+++ consistenthash.go
@@ -0,0 +1,141 @@
+// Reimplemantation of http://www.lexemetech.com/2007/11/consistent-hashing.html in Go
+
+package main
+
+import "fmt"
+import "crypto/md5"
+import "io"
+import "strconv"
+import "sort"
+import "log"
+
+const SEPERATOR = ":"
+
+type ConsistentHash struct {
+    //replicas indicates how many virtual points should be used per node and they are required to improve the distribution
+    NumberOfReplicas    int
+    // Circle
+    Circle              map[string]string
+    // Sorted list of hashes
+    Hashes              []string
+}
+
+// helper function for generating md5 hash
+func generateHash(input string) string {
+    key := md5.New()
+    _, err := io.WriteString(key, input)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return fmt.Sprintf("%x", key.Sum(nil))
+}
+
+// returns an initialized ConsistentHash
+func New(NumberOfReplicas int) *ConsistentHash {
+    ch := new(ConsistentHash)
+    ch.NumberOfReplicas = NumberOfReplicas
+    ch.Circle = make(map[string]string)
+    return ch
+}
+
+// adds a node to Circle
+func (ch *ConsistentHash) Add(node string) {
+    for i := 0; i < ch.NumberOfReplicas; i++ {
+        // use SEPERATOR for protecting ourselves from collisions. If we concatenate directly then following happens;
+        //
+        // We have 20 nodes which are represented by strings as "node1"..."node20" with 100 replicas for each node
+        ///
+        // Following "virtual nodes" are generated "node1":
+        // "node1" + 0 = "node10"
+        // ...
+        // "node1" + 10 = "node110"
+        // "node1" + 11 = "node111"
+        // ...
+        //
+        // At the same, following "virtual nodes" are generated for "node11":
+        // "node11" + 0 = "node110"
+        // "node11" + 1 = "node111"
+        // ...
+        hash := generateHash(node + SEPERATOR + strconv.Itoa(i))
+        ch.Circle[hash] = node
+        ch.Hashes = append(ch.Hashes, hash)
+    }
+    // keep Hashes sorted as we are going to use binary search on them
+    sort.Strings(ch.Hashes)
+}
+
+// removes a node from Circle
+func (ch *ConsistentHash) Remove(node string) {
+    for i := 0; i < ch.NumberOfReplicas; i++ {
+        hash := generateHash(node + ":" + strconv.Itoa(i))
+        delete(ch.Circle, hash)
+    }
+
+    // As long as I see Go has append but not remove...
+    ch.Hashes = ch.Hashes[:0]
+
+    for k,_ := range ch.Circle {
+        ch.Hashes = append(ch.Hashes, k)
+    }
+    // keep Hashes sorted as we are going to use binary search on them
+    sort.Strings(ch.Hashes)
+}
+
+// returns the index of node that holds given key
+func (ch *ConsistentHash) search(key string) (int) {
+    // use binary search to find a proper node for fiven key
+    index := sort.Search(len(ch.Hashes), func(i int) bool { return ch.Hashes[i] > key })
+
+    if index == len(ch.Hashes) {
+        index = 0
+    }
+
+    return index
+}
+
+// returns the node for given key
+func (ch *ConsistentHash) Get(key string) (string) {
+    index := ch.search(generateHash(key))
+    return ch.Circle[ch.Hashes[index]]
+}
+
+func main(){
+    numberOfReplicas := 32
+    keys := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+    distribution := func (ch *ConsistentHash, keys []string) (map[string]int) {
+        m := make(map[string]int)
+        for _, v := range keys {
+            m[ch.Get(v)]++
+        }
+        return m
+    }
+
+    ch := New(numberOfReplicas)
+    ch.Add("node1")
+    ch.Add("node2")
+    ch.Add("node3")
+
+    fmt.Println("With 3 nodes")
+    fmt.Printf("\t%v\n", distribution(ch, keys))
+
+    fmt.Println("Removing node1")
+    ch.Remove("node1")
+    fmt.Printf("\t%v\n", distribution(ch, keys))
+
+    fmt.Println("Removing node3")
+    ch.Remove("node3")
+    fmt.Printf("\t%v\n", distribution(ch, keys))
+
+    fmt.Println("Adding node3")
+    ch.Add("node3")
+    fmt.Printf("\t%v\n", distribution(ch, keys))
+
+    fmt.Println("Adding node1")
+    ch.Add("node1")
+    fmt.Printf("\t%v\n", distribution(ch, keys))
+
+    fmt.Println("Adding node4")
+    ch.Add("node4")
+    fmt.Printf("\t%v\n", distribution(ch, keys))
+}
